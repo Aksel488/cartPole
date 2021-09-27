8c94c458e28e5660826e75aa2d26f9114ee45615
Aksel488 <aksel.susegg@gmail.com>
2021-09-27 14:14:25 +0200

    8-bit rulesets

--- bit_string_manipulation.py
+++ bit_string_manipulation.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 
 # random.seed(0)
+
+n_rules = 100
+rules_length = 1000
+iterations = 200
 
 class BitString:
     def __init__(self, n_rules, rules_length):
@@ -18,11 +23,50 @@ class BitString:
         ''' copies a range of values from list A to B '''
         B[start:stop] = A[start:stop]
 
+    def next_generation(self):
+        '''
+        uses half the fittest in the population to create the next generation
+        with the uniform crossover algorithm.
+        Parrent A and B can be the same
+        '''
+        children = []
+        next_gen = []
+        for _ in range(int(len(self.rules)/2)):
+            # picks the parrents
+            max_fitnes = max(self.fitnes)
+            idx = self.fitnes.index(max_fitnes)
+            next_gen.append(self.rules[idx])
+            self.fitnes[idx] = 0 # sets the fitnes to 0 so it wont be choosen again
+
+        for i in range(len(next_gen)):
+            # creates the children
+            child = []
+            mask = [random.randint(0, 1) for _ in range(len(self.rules[0]))]
+            parrentA = next_gen[i]
+            parrentB = next_gen[random.randint(0, len(next_gen)-1)]
+            for j in range(len(parrentA)):
+                if (mask[j] == 1):
+                    child.append(parrentA[j])
+                elif (mask[j] == 0):
+                    child.append(parrentB[j])
+            children.append(child)
+
+            # print('parrentA:',parrentA)
+            # print('parrentB:',parrentB)
+            # print('mask    :',mask)
+            # print('child   :',child)
+        
+        for child in children:
+            next_gen.append(child)
+
+        self.rules = next_gen
+
+
     def mutate(self):
-        ''' every element has a 2% chance of flipping '''
+        ''' every element has a 1% chance of flipping '''
         for rule in self.rules:
             for i in range(len(rule)):
-                if (random.random() < 0.02):
+                if (random.random() < 0.01):
                     if (rule[i] == 0):
                         rule[i] = 1
                     else: 
@@ -32,37 +76,56 @@ class BitString:
     def fit(self):
         ''' loops until one set of rules is all 1-s '''
         epochs = 0
-        while True:
-            idx = self.fitnes.index(max(self.fitnes))
-            A = self.rules[idx]
-            B = self.rules[random.randint(0, len(self.rules) - 1)]
-            start = random.randint(0, len(self.rules[0]))
-            stop  = random.randint(0, len(self.rules[0]))
-            if (start > stop):
-                start, stop = stop, start
+        y_mean = []
+        y_max  = []
+        y_min  = []
+        x      = []
 
-            self.spalt(A, B, start, stop)
+        for _ in range(iterations):
+            max_fitnes = max(self.fitnes)
+            min_fitnes = min(self.fitnes)
+
+            # idx = self.fitnes.index(max_fitnes)
+            # A = self.rules[idx]
+            # B = self.rules[random.randint(0, len(self.rules) - 1)]
+            # start = random.randint(0, len(self.rules[0]))
+            # stop  = random.randint(0, len(self.rules[0]))
+            # if (start > stop):
+            #     start, stop = stop, start
+            # self.spalt(A, B, start, stop)
+
+            self.next_generation()
             self.mutate()
 
             for i in range(len(self.rules)):
                 self.fitnes[i] = self.getFitnes(self.rules[i])
             
-            if (epochs % 1000 == 0):
-                print(sum(self.fitnes))
+            mean_fitnes = np.mean(self.fitnes)
+            print('max fitnes: {} mean fitnes: {}'.format(max_fitnes, mean_fitnes))
+
+            ''' create plot points for mean and max '''
+            y_mean.append(mean_fitnes)
+            y_max.append(max_fitnes)
+            y_min.append(min_fitnes)
+            x.append(epochs)
+
+            
             epochs += 1
             if (max(self.fitnes) == len(self.rules[0])):
                 print("Finished after {} epochs".format(epochs))
                 break
-
-
+        mean_plot, = plt.plot(x, y_mean)
+        max_plot, = plt.plot(x, y_max)
+        min_plot, = plt.plot(x, y_min)
+        plt.legend([max_plot, mean_plot, min_plot], ["max", "mean", "min"])
+        plt.show()
 
     def show(self):
         for rule in self.rules:
             print(rule)
         print(self.fitnes)
-            # print(self.getFitnes(rule))
 
-bs = BitString(20, 30)
+bs = BitString(n_rules, rules_length)
 bs.show()
 bs.fit()
 bs.show()
